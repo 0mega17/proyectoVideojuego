56e004cc040746c013cc023fa67a6d9634c45ecc
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2025-12-08 17:21:20 -0500

    Vaciar sala y actualizar estado de la sala = 0 en finalizar juego

--- controllers/finalizar_juego.php
+++ controllers/finalizar_juego.php
@@ -10,16 +10,26 @@ $errores = [];
 // Codigo de la sala
 $codigoSala = $_POST["codigoSala"];
 $accion = "finalizar";
+$estado = 0;
 
 // Update en la accion de la sala
 try {
-    $sqlUpdate = "UPDATE codigos SET accion = :accion WHERE codigo = :codigoSala ";
+    $sqlUpdate = "UPDATE codigos SET accion = :accion, estado = :estado WHERE codigo = :codigoSala ";
     $updateEstado = $mysql->getConexion()->prepare($sqlUpdate);
     $updateEstado->bindParam("codigoSala", $codigoSala, PDO::PARAM_INT);
     $updateEstado->bindParam("accion", $accion, PDO::PARAM_STR);
+    $updateEstado->bindParam("estado", $estado, PDO::PARAM_INT);
     $updateEstado->execute();
 } catch (PDOException $e) {
     $errores[] = "Ocurrio un error en el update de la sala..." . $e->getMessage();
+}
+
+try {
+    $sqlTruncate = "TRUNCATE aprendices";
+    $consultaTruncate = $mysql->getConexion()->prepare($sqlTruncate);
+    $consultaTruncate->execute();
+} catch (PDOException $e) {
+    $errores[] = "Ocurrio un error en el truncate..." . $e->getMessage();
 }
 
 
